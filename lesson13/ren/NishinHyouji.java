593a811b61879d8a430d129ddedaf9de69927d59
tk84 <bjjt1290058@gn.iwasaki.ac.jp>
2021-07-15 13:16:36 +0900

    7/15

---
+++ lesson13/ren/NishinHyouji.java
@@ -0,0 +1,18 @@
+package lesson13.ren;
+
+interface Observer {
+  public void update(Subject s);
+}
+
+class NishinHyouji implements Observer {
+  public void update(Subject s) {
+    print(((Suuchi) s).getValue());
+  }
+
+  private void print(int n) {
+    System.out.println(n + "を 2 進数で表示します");
+    //
+    // ここに 2 進数表示処理を作成する
+    //
+  }
+}
